7f5caea33d60852eeb6a06dd569c9ee1b79a143f
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2024-12-12 10:20:05 +0100

    build: add display functions

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let operator, a, b;
-let display = "";
 let displayReset = false;
 
 const digits = document.querySelectorAll(".digit-btn");
@@ -7,23 +6,40 @@ const operators = document.querySelectorAll(".operator-btn");
 const currentDisplay = document.querySelector(".currentDisplay");
 const previousDisplay = document.querySelector(".previousDisplay");
 
+const clearButton = document.querySelector("#clearbtn");
+const deleteButton = document.querySelector("#deletebtn");
+const equalButton = document.querySelector("#equalbtn")
+
+
+// --------- DISPLAY FUNCTIONS ---------
+
 digits.forEach((digit) => {
   digit.addEventListener("click", () => appendToDisplay(digit.textContent));
 })
 
-// operators.forEach((operator) => {
-//   operator.addEventListener("click", () => )
-// })
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => appendToDisplay(operator.textContent));
+})
 
 function appendToDisplay(value) {
   if (currentDisplay === "0" || previousDisplay)
   currentDisplay.textContent += value;
 }
 
+clearButton.addEventListener("click", () => clearDisplay());
 
 function clearDisplay() {
-
+  a = 0;
+  b = 0;
+  operator = "";
+  currentDisplay.textContent = "";
+  previousDisplay.textContent = "";
 }
+
+equalButton.addEventListener("click", () => operate())
+
+
+// --------- MATHS FUNCTIONS ---------
 
 function add(a, b) {
   return a + b;
