cd6546004a0054d4ee06852e025fe5344bd10657
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2024-12-18 14:54:05 +0100

    feat: add main operation system on pressing equal
    Created functions to store the operator and compute the right operation
    on pressing equal button based on operator sign (setOperator() &
    operate() functions). For that, added IDs to HTML elements.
    
    Refactored display functions which weren't taking into account the
    difference between numeric variables and strings relative to variables
    displayed on screen, and added updateDisplay() function to move current
    display to previous display upon choosing an operator.
    
    Added parse methods to operate() to convert the a and b strings into
    numbers, as well as the result, before converting the result back to a
    number.
    
    Added function to handle decimal numbers.

--- script.js
+++ script.js
@@ -1,49 +1,112 @@
-let operator, a, b;
-let displayReset = false;
-
-const digits = document.querySelectorAll(".digit-btn");
-const operators = document.querySelectorAll(".operator-btn");
-const currentDisplay = document.querySelector(".currentDisplay");
-const previousDisplay = document.querySelector(".previousDisplay");
-
-const clearButton = document.querySelector("#clearbtn");
-const deleteButton = document.querySelector("#deletebtn");
-const equalButton = document.querySelector("#equalbtn")
-
+let operator = "";
+let a = "";
+let b = "";
+let previousOperation = "";
+let result = 0;
 
 // --------- DISPLAY FUNCTIONS ---------
 
+const digits = document.querySelectorAll(".digit-btn");
+const currentDisplay = document.querySelector(".currentDisplay");
+const previousDisplay = document.querySelector(".previousDisplay");
+
 digits.forEach((digit) => {
-  digit.addEventListener("click", () => appendToDisplay(digit.textContent));
-})
+  digit.addEventListener("click", () => {
+    if (previousOperation == "") {
+      a += digit.value;
+      currentDisplay.textContent = a;
+    } else if (previousOperation.charAt(previousOperation.length-2) == "=") {
+      a = "";
+      a += digit.value;
+      currentDisplay.textContent = a;
+      previousOperation = "";
+      previousDisplay.textContent = previousOperation;
+    } else {
+      b += digit.value;
+      currentDisplay.textContent = b;
+    }
+  });
+});
 
-operators.forEach((operator) => {
-  operator.addEventListener("click", () => appendToDisplay(operator.textContent));
-})
-
-function appendToDisplay(value) {
-  if (currentDisplay === "0" || previousDisplay)
-  currentDisplay.textContent += value;
+function setOperator(sign, name) {
+  if (a.length != 0 && (previousOperation == "" || previousOperation != "" && b.length == 0)) {
+    updateDisplay(sign, name);
+  } else if (a.length !== 0 && b.length !== 0 && previousOperation !== "") {
+    operate(operator, a, b);
+    updateDisplay(sign, name);
+  }
 }
 
+function updateDisplay(sign, name) {
+  operator = name;
+  previousOperation = a + sign;
+  previousDisplay.textContent = previousOperation;
+}
+
+const decimalButton = document.querySelector("#decimal");
+decimalButton.addEventListener("click", () => handleDecimal())
+function handleDecimal() {
+  if (previousOperation == "") {
+    if (!a.includes(".") && a.length >= 1) {
+      a += ".";
+      currentDisplay.textContent = a;
+    }
+  } else {
+    if (!b.includes (".") && b.length >= 1) {
+      b += ".";
+      currentDisplay.textContent = b;
+    }
+  }
+}
+
+// --------- CLEAR FUNCTIONS ---------
+
+const clearButton = document.querySelector("#clearbtn");
+clearButton.addEventListener("click", () => clearDisplay());
 function clearDisplay() {
-  a = 0;
-  b = 0;
-  operator = "";
+  a = "";
+  b = "";
+  previousOperation = "";
+  result = 0;
   currentDisplay.textContent = "";
   previousDisplay.textContent = "";
 }
 
+const deleteButton = document.querySelector("#deletebtn");
+deleteButton.addEventListener("click", () => deleteLastChar())
 function deleteLastChar() {
-  currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+  if (previousOperation == "" && a.length >= 1) {
+    a = a.slice(0, -1);
+    currentDisplay.textContent = a;
+  } else if (previousOperation != "" && b.length >= 1) {
+    b = b.slice(0, -1);
+    currentDisplay.textContent = b;
+  }
 }
 
-clearButton.addEventListener("click", () => clearDisplay());
-deleteButton.addEventListener("click", () => deleteLastChar())
-equalButton.addEventListener("click", () => operate())
-
-
 // --------- MATHS FUNCTIONS ---------
+
+const addButton = document.querySelector("#add");
+addButton.addEventListener("click", () => setOperator("+", "add"));
+
+const subtractButton = document.querySelector("#subtract");
+subtractButton.addEventListener("click", () => setOperator("-", "subtract"));
+
+const multiplyButton = document.querySelector("#multiply");
+multiplyButton.addEventListener("click", () => setOperator("*", "multiply"))
+
+const divideButton = document.querySelector("#divide");
+divideButton.addEventListener("click", () => setOperator("/", "divide"));
+
+const equalButton = document.querySelector("#equalbtn");
+equalButton.addEventListener("click", () => {
+  if (a.length != 0 && b.length != 0 && previousOperation.charAt(previousOperation.length-1) != "=") {
+    previousOperation = previousOperation + b + "=";
+    previousDisplay.textContent = previousOperation;
+    operate(operator, a, b);
+    operator = "";
+  }
+})
 
 function add(a, b) {
   return a + b;
@@ -61,20 +124,30 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
+function operate() {
+  a = parseFloat(a);
+  b = parseFloat(b);
+
   switch (operator) {
-    case "+":
-      return add(a, b);
+    case "add":
+      result = add(a, b);
       break;
-    case "-":
-      return subtract(a, b);
+    case "subtract":
+      result = subtract(a, b);
       break;
-    case "*":
-      return multiply(a, b);
+    case "multiply":
+      result = multiply(a, b);
       break;
-    case "/":
-      if (b === 0) return null
-      else return divide(a, b);
+    case "divide":
+      if (b === 0) {
+        alert("Error! You cannot divide by 0.")
+      }
+      else result = divide(a, b);
       break;
-  }
+  }  
+
+  result = parseFloat(result.toFixed(3));
+  currentDisplay.textContent = result;
+  a = result.toString();
+  b = "";
 }
